8624aeaae919cab84ff6d42843956be118968f76
chaman0011 <121281059+chaman0011@users.noreply.github.com>
2025-03-31 18:19:03 +0530

    Add files via upload

---
+++ A1task1.py
@@ -0,0 +1,22 @@
+
+num1 = input("Enter the first number ")
+
+num2= input("Enter the second number ")
+
+
+# addition of two numbers
+
+print("addition :" ,int(num1)+int(num2))
+
+
+# Subtraction of two number
+
+print("Subtraction :",int (num1)-int (num2))
+
+#   Multiplication of two numbers
+
+print ("Multiplication :",int (num1)*int (num2))
+
+#Division of two number
+
+print("Division of two numbers :", int (num1)/int(num2))
